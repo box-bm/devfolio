274d3a59cbf8fc970f031ba67d68bac4378eaeb1
Brandon Manzo <57267719+box-bm@users.noreply.github.com>
2026-01-08 00:50:22 -0600

    feat: added support for i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.about": "About",
+    "nav.project": "Projects",
+    "nav.contact": "Contact",
+  },
+  es: {
+    "nav.about": "Sobre Mi",
+    "nav.project": "Proyectos",
+    "nav.contact": "Contacto",
+  },
+};
