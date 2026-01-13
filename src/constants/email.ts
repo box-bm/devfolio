48e5e705901ad23f2f3ff926515a3f18f1377428
box-bm <57267719+box-bm@users.noreply.github.com>
2026-01-13 07:31:34 -0600

    feat: added links

---
+++ src/constants/email.ts
@@ -0,0 +1,1 @@
+export const email = "brandong.manzog@gmail.com";
---
+++ src/constants/github.ts
@@ -0,0 +1,2 @@
+export const githubProfile = "https://github.com/box-bm";
+export const githubRepos = "https://github.com/box-bm?tab=repositories";
