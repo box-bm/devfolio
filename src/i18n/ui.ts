a5f7170edeeefccf912a15f0e11c65c83f0d1702
box-bm <57267719+box-bm@users.noreply.github.com>
2026-01-17 09:19:36 -0600

    feat: finished terms and conditions

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,29 +7,29 @@ export const defaultLang = "en";
 
 export const ui = {
   en: {
+    // header
     "nav.about": "About",
     "nav.project": "Projects",
     "nav.contact": "Contact",
-    legal: {
-      privacy: "Privacy Policy",
-      terms: "Terms of Service",
-      languages: "Languages",
-      app: "App",
-      version: "Version",
-      effectiveDate: "Effective Date",
-    },
+    //legal
+    "legal.privacy": "Privacy Policy",
+    "legal.terms": "Terms of Service",
+    "legal.languages": "Languages",
+    "legal.app": "App",
+    "legal.version": "Version",
+    "legal.effectiveDate": "Effective Date",
   },
   es: {
+    // header
     "nav.about": "Sobre Mi",
     "nav.project": "Proyectos",
     "nav.contact": "Contacto",
-    legal: {
-      privacy: "Política de Privacidad",
-      terms: "Términos de Servicio",
-      languages: "Idiomas",
-      app: "Aplicación",
-      version: "Versión",
-      effectiveDate: "Fecha de Vigencia",
-    },
+    // legal
+    "legal.privacy": "Política de Privacidad",
+    "legal.terms": "Términos de Servicio",
+    "legal.languages": "Idiomas",
+    "legal.app": "Aplicación",
+    "legal.version": "Versión",
+    "legal.effectiveDate": "Fecha de Vigencia",
   },
 };
