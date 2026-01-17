c65a0964ea2921c17aed06402c33027941de0c60
box-bm <57267719+box-bm@users.noreply.github.com>
2026-01-17 15:52:36 -0600

    feat: added domain and workflow to deploy pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://boxdev.me",
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "en",
